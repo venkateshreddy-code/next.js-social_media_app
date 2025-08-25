50e6dc923f7479966f1e2850b0fb8f3d9a8a37cd
venky178 <venkateshreddyningam@gmail.com>
2025-08-24 23:51:56 -0400

    login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,29 @@
+// app/page.tsx
 import CreatePost from "@/components/CreatePost";
 import WhoToFollow from "@/components/WhoToFollow";
-import { currentUser } from "@clerk/nextjs/server";
 
 import { getPosts } from "@/actions/post.action";
 import { getDbUserId } from "@/actions/user.action";
 import PostCard from "@/components/PostCard";
 
 export default async function Home() {
-  const user = await currentUser();
   const posts = await getPosts();
   const dbUserId = await getDbUserId();
-
-
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
       <div className="lg:col-span-6">
-        {user ? <CreatePost /> : null}
+        {/* Always render CreatePost, it will check auth internally via useUser */}
+        <CreatePost />
 
         <div className="space-y-6">
           {posts.map((post) => (
-         
-            <PostCard key = {post.id} post ={post} dbUserId={dbUserId}/>
+            <PostCard key={post.id} post={post} dbUserId={dbUserId} />
           ))}
         </div>
       </div>
-      <div className="hidden lg:block lg:col-span-4 sticky top-20">
 
+      <div className="hidden lg:block lg:col-span-4 sticky top-20">
         <WhoToFollow />
       </div>
     </div>
